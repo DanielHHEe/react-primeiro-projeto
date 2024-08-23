2d7c002883bed20d06f0f516655b7cf6ce941078
Daniel <santosherenio76@gmail.com>
2024-08-23 15:29:29 -0300

    Subindo repositorio

---
+++ src/componentes/questions.ts
@@ -0,0 +1,125 @@
+import { Question } from "./Question";
+
+export const questions: Question[] = [
+    {
+        question: 'Qual Região do Brasil está localizado o Espirito santo ?',
+        options: [
+            'Norte',
+            'Nordeste',
+            'centroeste',
+            'suldeste'
+        ],
+        answer: 3
+    },
+    {
+        question: 'Qual país não é considerado da América latina ?',
+        options: [
+            'Colombia',
+            'Equador',
+            'Honduras',
+            'Brasil'
+
+        ],
+        answer: 2
+
+    },
+    {
+        question: 'Qual o maior rio em extensão territorial do mundo  ?',
+        options: [
+            'Rio Amazonas',
+            'Rio missisipi',
+            'Rio nilo',
+            'Rio Yangtze'
+
+        ],
+        answer: 2
+
+    },
+    {
+        question: 'Em qual continente fica localizada a República Tcheca ?',
+        options: [
+            'Oceania',
+            'Europa',
+            'África',
+            'America central'
+
+        ],
+        answer: 1
+
+    },
+    {
+        question: 'Qual a capital do Estados Unidos ?',
+        options: [
+            'washington ',
+            'Nova York',
+            'Los Angeles',
+            'Las Vegas'
+
+        ],
+        answer: 0
+
+    },
+    {
+        question: 'Onde está localizado a cidade de Otawwa ?',
+        options: [
+            'Japão ',
+            'Canadá',
+            'China',
+            'Russia'
+
+        ],
+        answer: 1
+
+    },
+    {
+        question: 'A torre de Paris se deu inicío a construção em que ano  ?',
+        options: [
+            '1887 ',
+            '1896',
+            '1903',
+            '1917'
+
+        ],
+        answer: 0
+
+
+    },
+    {
+        question: 'Em que ano ocorreu o ataque a torre gemêas ?',
+        options: [
+            '15 de agosto de 2003 ',
+            '6 de setembro de 1998',
+            '1 de janeiro de 2004',
+            '11 de setembro de 2001'
+
+        ],
+        answer: 3
+
+    },
+    {
+        question: 'Qual foi o primeiro presidente do Brasil ?',
+        options: [
+            'Floriano Peixoto',
+            'Getúlio Vargas',
+            'marechal Deodoro da Fonseca',
+            'Prudente de Morais'
+
+        ],
+        answer: 2
+
+    },
+    {
+        question: 'Qual foi o primeiro estado do Brasil que os portuguêses chegaram ?',
+        options: [
+            'Maranhão',
+            'Pernambuco',
+            'Rio de Janeiro',
+            'Bahia'
+
+        ],
+        answer: 3
+
+    },
+    
+
+]
